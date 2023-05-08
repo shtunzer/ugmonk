34502775b88d6e6078b8eaa398048ffca7acc94a
Denis <130508415+shtunzer@users.noreply.github.com>
2023-05-08 22:03:09 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 // MENU
 
-// document.getElementById('burger-btn-open').addEventListener('click', function(){
-//     document.getElementById('burger__menu').classList.add('--show');
-// });
-// document.getElementById('burger-btn-hide').addEventListener('click', function(){
-//     document.getElementById('burger__menu').classList.remove('--show');
-// });
+document.getElementById('burger-btn-open').addEventListener('click', function(){
+    document.getElementById('burger__menu').classList.add('--show');
+});
+document.getElementById('burger-btn-hide').addEventListener('click', function(){
+    document.getElementById('burger__menu').classList.remove('--show');
+});
 
 // TABS
 
